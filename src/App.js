12f2d23685b37cb0ffd20b37c79f7c6092a9465e
MihailMilyuk <milyukmihail@gmail.com>
2020-12-31 18:51:45 +0300

    Some style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import './bootstrap/main.css';
 
 const el = () =>
-    <div>
-        <h1>Hello World!</h1>
+    <div className="container">
+        <div className="row">
+            <div className="align-self-center text-center mt-20 ml-45">
+                <div className="col">
+                    <h1>Todo List</h1>
+                </div>
+                <div className="col">
+                    <h2>Tested</h2>
+                </div>
+            </div>
+        </div>
     </div>
 
 export default el;
